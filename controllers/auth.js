da0b4353f01c819f04c7ec9151d2d8bbc44a7218
Elborn666 <elborn18@gmail.com>
2023-08-17 18:08:56 +0300

    тестова перевірка

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,13 @@
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const gravatar = require('gravatar');
+const path = require('path')
+const fs = require('fs/promises');
 
 const { HttpError } = require('../helpers');
-const { SECRET_KEY } = require('../config')
+const { SECRET_KEY } = require('../config');
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const { User } = require('../models/user');
 
@@ -13,8 +18,8 @@ const register = async (req, res, next) => {
     
 
     const hashPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await User.create({ ...req.body, rassword: hashPassword });
+    const avatarURL = gravatar.url(email);
+    const newUser = await User.create({ ...req.body, rassword: hashPassword, avatarURL});
 
     res.status(201).json({
         name: newUser.name,
@@ -63,9 +68,24 @@ const logout = async (req, res) => {
     })
 }
 
+const updateAvatar = async (req, res) => {
+    const {_id} = req.user;
+    const {path: tempUpload, orirginalname} = req.file;
+    const filename = `${_id}_${orirginalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, {avatarUrl});
+
+    res.json({
+        avatarURL,
+    })
+}
+
 module.exports = {
     register,
     login,
     getCurrent,
     logout,
+    updateAvatar,
 }
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateBody = require('./validateBody');
 const isVakidId = require('./isValidId');
 const authenticate = require('./authenticate');
+const upload = require('./upload');
 
 module.exports = {
     validateBody,
     isVakidId,
     authenticate,
+    upload,
 }
